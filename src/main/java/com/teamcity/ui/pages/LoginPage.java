84f27aee0c213e31b93873ddab6d4e8e5ca9cef3
amkaznov <amkaznov@gmail.com>
2024-05-28 17:21:52 +0700

    Отладил запуск тестов

--- src/main/java/com/teamcity/ui/pages/LoginPage.java
+++ src/main/java/com/teamcity/ui/pages/LoginPage.java
@@ -2,6 +2,7 @@ package com.teamcity.ui.pages;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import com.teamcity.api.config.Config;
 import com.teamcity.api.models.User;
 import com.teamcity.ui.Selectors;
 import lombok.Getter;
@@ -35,6 +36,7 @@ public class LoginPage extends Page{
     }
     public LoginPage loginSuperUser ()  {
         waitUntilPageIsLoaded();
+        passwordInput.sendKeys(Config.getProperty("superUserToken"));
         clickOnSubmit();
         waitUntilPageIsLoaded();
         return this;
